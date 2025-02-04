61561337d56211713adb36f7c2cba49820f9079b
ivanmilicevic <93290646+ivanmeda993@users.noreply.github.com>
2025-02-04 01:04:51 +0100

    feat: add get_wedding_by_invite_code function

---
+++ supabase/migrations/20250203001002_add_get_wedding_by_invite_code.sql
@@ -0,0 +1,27 @@
+/*
+  # Add get_wedding_by_invite_code function
+
+  1. Function
+    - Takes invite code as parameter
+    - Returns wedding details if found
+    - Used for validating invite links
+*/
+
+-- Create function to get wedding by invite code
+CREATE OR REPLACE FUNCTION get_wedding_by_invite_code(code text)
+RETURNS TABLE (
+  id uuid,
+  bride_name text,
+  groom_name text
+) 
+SECURITY DEFINER
+SET search_path = public
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT w.id, w.bride_name, w.groom_name
+  FROM weddings w
+  WHERE w.invite_code = code::uuid;
+END;
+$$; 
